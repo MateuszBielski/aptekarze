6d93535fda86cab70466b089ce62414bdcb03063
Mateusz Bielski <mateo.bass@interia.pl>
2019-08-02 13:12:08 +0200

    tests register login add find delete user seem to working

--- tests/MemberUserControllerTest.php
+++ tests/MemberUserControllerTest.php
@@ -4,6 +4,7 @@ namespace App\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use App\Repository\MemberUserRepository;
+use App\Entity\MemberUser;
 
 class MemberUserControllerTest extends WebTestCase
 {
@@ -29,61 +30,49 @@ class MemberUserControllerTest extends WebTestCase
     //     $this->memRep = $memberUserRepository;
     //     $this->em = $entityManager;
     // }
-
-    public function testPersistNewMemberUser()
-    {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/member/user/new');
-        $form = $crawler->selectButton('submit-form')->form();
-
-        $form['member_user[firstName]'] = 'testImie';
-        $form['member_user[surname]'] = 'testNazwisko';
-        $form['member_user[telephone]'] = 'testTelefon';
-        $form['member_user[job]']->select(6);//nie ma znaczenia
-
-        //powinno przekierować do mem us Index
-        // $crawler = $client->submit($form);
-        // $this->assertResponseIsSuccessful();<-**********odblokować gdy zadziała usuwanie
-
-        $container = self::$container;
-        $memRep = $container->get('App\Repository\MemberUserRepository');
-        $mu = $memRep->findOneBy(['username' => "testImietestNazwiskotestTelefon",]);
-
-        $this->memUsId = $mu->getId();
-        
-        $this->assertGreaterThan(0,$this->memUsId);
-        
-        
-        /* usunięcie */
-        // if($mu != null){
-        //     $entityManager = $container->get('doctrine.orm.entity_manager');
-        //     $entityManager->remove($mu);
-        //     $entityManager->flush();
-        // }
-
-
-    }
+  
     
-    
-    public function testRegisterNewUser()
+    public function testAddAndDeleteUsers()
     {   
+        
         $client = static::createClient();
-        /*
-        $crawler = $client->request('GET', '/register');
+        $userAdmin = $this->NewMemberWith('7655');
+        $otherUser = $this->NewMemberWith('5325');
+        $this->RegisterUserAsAdmin($userAdmin,$client);
+        $this->AddNewUser($otherUser,$client);
+        $this->LoginAs($userAdmin,$client);
 
-        $num = '7757';
+        $this->FindUserAndDelete($otherUser,$client);
+        $this->FindUserAndDelete($userAdmin,$client);
+    }
+
+    private function NewMemberWith($num)
+    {
+        $mu= new MemberUser();
+        $mu->setSurname('test'.$num.'Nazwisko');
+        $mu->setFirstName('test'.$num.'Imie');
+        $mu->setPassword('password'.$num.'Test');
+        $mu->setUserName('testUN'.$num);
+        $mu->setTelephone($num);
+        return $mu;
+    }
+
+    private function RegisterUserAsAdmin(MemberUser $user,$client)
+    {
+        $crawler = $client->request('GET', '/register');
+        
         
         $form = $crawler->selectButton('submit-form')->form();
-        $form['registration_form[firstname]'] = 'test'.$num.'AdminImie';
-        $form['registration_form[surname]'] = 'testAdmin'.$num.'Nazwisko';
-        $form['registration_form[username]'] = 'testAdmin'.$num;
-        $form['registration_form[plainPassword]'] = 'passwordTest';
+        $form['registration_form[firstname]'] = $user->getFirstName();
+        $form['registration_form[surname]'] = $user->getSurname();
+        $form['registration_form[username]'] = $user->getUsername();
+        $form['registration_form[plainPassword]'] = $user->getPassword();
         $crawler = $client->submit($form);
         
         $container = self::$container;
         $memRep = $container->get('App\Repository\MemberUserRepository');
-        $toFind = "testAdmin".$num;
-        $mu = $memRep->findOneBy(['username' => $toFind,]);
+        
+        $mu = $memRep->findOneBy(['username' => $user->getUsername(),]);
         
         $mu->setRoles(["ROLE_ADMIN"]);
         
@@ -92,21 +81,47 @@ class MemberUserControllerTest extends WebTestCase
         $entityManager->flush();
         
         $this->assertResponseRedirects('/member/user/', 302);
+    }
 
-        $toFind = 'testAdmin'.$num.'Nazwisko';
-        $link = $crawler->filter('a:contains("'.$toFind.")')->link();
-        $crawler = $client->click($link);
+    private function AddNewUser(MemberUser $user,$client)
+    {
+        $crawler = $client->request('GET', '/member/user/new');
+        $form = $crawler->selectButton('submit-form')->form();
+
+
         
-        $this->assertResponseIsSuccessful();
-        */
+        $form['member_user[firstName]'] = $user->getFirstName();;
+        $form['member_user[surname]'] = $user->getSurname();
+        $form['member_user[telephone]'] = $user->getTelephone();
+        $form['member_user[job]']->select(6);//nie ma znaczenia
+
+        //powinno przekierować do mem us Index
+        $crawler = $client->submit($form);
+        // $this->assertResponseIsSuccessful();<-**********odblokować gdy zadziała usuwanie
+
+        $user->createTempUsername();
+        $container = self::$container;
+        $memRep = $container->get('App\Repository\MemberUserRepository');
+        $mu = $memRep->findOneBy(['username' => $user->getUsername(),]);
+
+        $this->memUsId = $mu->getId();
+        
+        $this->assertGreaterThan(0,$this->memUsId);
+    }
+
+    private function LoginAs(MemberUser $admin,$client)
+    {
         $crawler = $client->request('GET', '/login');
-        $form = $crawler->selectButton('form')->form();//->selectButton('submit')
-        $form['username'] = 'użytkregister';
-        $form['password'] = '87654321';
-        
-        $toFind = 'testAdminNazwisko';
+        $form = $crawler->selectButton('submit_login')->form();//przycisk musi mieć np: name="submit_login
+        $form['username'] = $admin->getUsername();
+        $form['password'] = $admin->getPassword();
+        $crawler = $client->submit($form);
+    }
+
+    private function FindUserAndDelete($userToDelete,$client)
+    {
         $crawler = $client->request('GET', '/member/user/');
-        $link = $crawler->filter('a:contains("'.$toFind.'")')
+        $link = $crawler->filter('a:contains("'.$userToDelete->getSurname().'")')
         ->eq(0)
         ->link();
         
@@ -126,10 +141,9 @@ class MemberUserControllerTest extends WebTestCase
         $form = $crawler->selectButton('submit-delete')->form();
         $crawler = $client->submit($form);
 
-        // $this->assertResponseRedirects('/member/user/', 302);
-
+        $this->assertResponseRedirects('/member/user/', 302);
     }
 
-
+    
     /*zrobić index i indexAjax podobnie ze składkami */
 }
